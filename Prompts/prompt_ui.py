8afc4678df77f309739bd6f61046164e4dcc79e0
RAJENDRA <rajendrajfc1021@gmail.com>
2025-05-23 00:36:39 +0530

    tinyllama promt fixed

--- Prompts/prompt_ui.py
+++ Prompts/prompt_ui.py
@@ -11,7 +11,8 @@ load_dotenv()
 
 model_path = "../tinyllama"
 tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
-model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True)
+# model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True)
+model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True, low_cpu_mem_usage=False)
 
 pipe = pipeline(
     "text-generation",
@@ -24,15 +25,19 @@ pipe = pipeline(
 )
 
 llm = HuggingFacePipeline(pipeline=pipe)
+generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 
 
-st.header("Research Assistant")
+st.header(" Assistant")
 
-user_input = st.text_area("Enter your research question here:")
+user_input = st.text_area("Enter your  question here:")
 
 
 
 if st.button("Submit"):
-    result = llm.invoke(user_input)
-    st.write(result)
+    prompt = f"Question: {user_input.strip()}\nAnswer:"
+    result = generator(prompt, max_new_tokens=50, do_sample=False)
+    answer = result[0]['generated_text'].replace(prompt, '').strip()
+    st.write(answer)
+
